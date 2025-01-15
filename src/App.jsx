52b4ef0b05c42efbc9ed3c5bbfed836dc7a25bcb
Adarsh <adarshtrip2306@gmail.com>
2025-01-15 17:25:28 +0530

    first react page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
-import MyButton from "./components/Button";
 
 function App() {
 	return (
-		<>
-			<h1>hello, this is adarsh tripathi,</h1>
-			<h2>Im learning react</h2>
-			<MyButton />
-		</>
+		<main>
+			<img src="/images/react.svg" width="100px" />
+			<h1> First React Page</h1>
+			<h2>Here We will dicuss Facts of react</h2>
+			<ul>
+				<li>Was developed in 2013</li>
+				<li>Was originally created by Jordan Walke</li>
+				<li>Has well over 100k stars on GitHub</li>
+				<li>Is maintained by Meta</li>
+			</ul>
+		</main>
 	);
 }
 
