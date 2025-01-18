1e5b342dcee9c45a4767886cd6e52ec21fd54cbc
Adarsh <adarshtrip2306@gmail.com>
2025-01-18 23:11:36 +0530

    first react app completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import MyFooter from "./components/Footer";
-import MyHeader from "./components/header";
+import MyHeader from "./components/Header";
 
 import Mypage from "./components/Page";
 import MyNav from "./components/Nav";
